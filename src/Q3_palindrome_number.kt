4fd77660b1ceefe187a3103c71a4deadf3009aeb
Abhishek (Desktop) <1171abhishek.com>
2025-02-26 07:13:45 +0530

    Implemented palindrome number check using mathematical approach

---
+++ src/Q3_palindrome_number.kt
@@ -0,0 +1,29 @@
+/* ******* Question *******
+Write a function that checks if a given integer x is a palindrome number. A number is considered a
+palindrome if it reads the same forward and backward. */
+
+/**
+ * % 10 -> gives the last digit
+ * / 10 -> removes the last digit
+ */
+
+
+fun isPalindrome(x: Int):Boolean {
+    var num = x
+    if (num < 0) return false
+    var reversed = 0
+    while (num != 0) {
+        val lastDigit = num % 10
+        reversed = reversed * 10 + lastDigit
+        num /= 10
+    }
+
+    return x == reversed
+
+}
+
+
+fun main() {
+    println(isPalindrome(121))
+}
+

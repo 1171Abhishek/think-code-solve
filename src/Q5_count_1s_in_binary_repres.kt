7603b3c811c0dd5b7454f90f2c808f661b633555
Abhishek(Laptop) <1171abhishek@gmail.com>
2025-03-03 13:48:36 +0530

    Q5 added

---
+++ src/Q5_count_1s_in_binary_repres.kt
@@ -0,0 +1,18 @@
+fun countOnesInBinary(n: Int): Int {
+    var num = n
+    var count = 0
+
+    while (num > 0) {
+        if (num % 2 == 1) {  // Check if last digit is 1
+            count++
+        }
+        num /= 2  // Remove last digit
+    }
+    return count
+}
+
+fun main() {
+    print("Enter a number: ")
+    val userInput = readln().toInt()
+    println("Number of 1s in binary: ${countOnesInBinary(userInput)}")
+}
